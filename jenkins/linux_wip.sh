ec0254abdefb2261420ce1fd2a772102f31c36bd
Jelani <jelani.brandon@microsoft.com>
2017-05-05 10:30:46 -0700

    Running e2e test on the WIP jobs

--- jenkins/linux_wip.sh
+++ jenkins/linux_wip.sh
@@ -6,6 +6,6 @@ build_root=$(cd "$(dirname "$0")/.." && pwd)
 cd $build_root
 
 # -- C --
-./build_all/linux/build.sh --run-unittests --wip-use-c2d-amqp-methods "$@" #-x 
+./build_all/linux/build.sh --run-unittests --run-e2e-tests --wip-use-c2d-amqp-methods "$@" #-x 
 [ $? -eq 0 ] || exit $?
 
