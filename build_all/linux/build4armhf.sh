ff22509db8c92c652b0b905822e974b3fb266356
Juergen Kosel <juergen.kosel@softing.com>
2017-08-15 10:43:42 +0200

    Correct build4armhf.sh script to find stdint.h and stdbool.h
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>

--- build_all/linux/build4armhf.sh
+++ build_all/linux/build4armhf.sh
@@ -5,22 +5,4 @@
 
 repo_root=$(cd "$(dirname "$0")/../.." && pwd)
 
-# Also assuming that the current working directory is the desired directory for the build
-cmake -DCMAKE_TOOLCHAIN_FILE=${repo_root}/build_all/linux/cmake_toolchain_file_armhf_linux \
-      -DOPENSSL_ROOT_DIR=$ARMHF_SYSROOT_DIR/usr \
-      -DOPENSSL_INCLUDE_DIR=$ARMHF_SYSROOT_DIR/usr/include \
-      -DOPENSSL_LIBRARIES="-L$ARMHF_SYSROOT_DIR/usr/lib/arm-linux-gnueabihf -lssl -lcrypto" \
-      -Drun_e2e_tests:BOOL=OFF \
-      -Drun_longhaul_tests=OFF \
-      -Duse_amqp:BOOL=ON \
-      -Duse_http:BOOL=ON \
-      -Duse_mqtt:BOOL=ON \
-      -Duse_installed_dependencies:BOOL=ON \
-      -Ddont_use_uploadtoblob:BOOL=ON \
-      -Drun_unittests:BOOL=OFF \
-      -Dbuild_python:STRING=OFF \
-      -Dbuild_javawrapper:BOOL=OFF \
-      -Dno_logging:BOOL=OFF \
-      -Dwip_use_c2d_amqp_methods:BOOL=OFF \
-      -Dazure_c_shared_utility_DIR=${repo_root}/c-utility/configs \
-      "$repo_root"
+$(dirname "$0")/build.sh --toolchain-file cmake_toolchain_file_armhf_linux -cl --sysroot=$ARMHF_SYSROOT_DIR -cl -L${ARMHF_SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf
