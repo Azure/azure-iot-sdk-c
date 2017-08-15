0a0c2d91fd09b411abc3e47bea52fcda13e61435
Juergen Kosel <juergen.kosel@softing.com>
2017-08-15 12:47:56 +0200

    Rework build4armhf script to create Makefiles in the current working directory
    Creating Makefiles in the current working directory makes it easier to
    integrate the sdk in another project.
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>

--- build_all/linux/build4armhf.sh
+++ build_all/linux/build4armhf.sh
@@ -3,6 +3,24 @@
 # Assuming that the variable ARMHF_SYSROOT_DIR points to the system root
 # of the host system with requiered libraries and headers.
 
-repo_root=$(cd "$(dirname "$0")/../.." && pwd)
+script_dir=$(cd "$(dirname "$0")" && pwd)
+build_root=$(cd "${script_dir}/../.." && pwd)
+log_dir=$build_root
+run_e2e_tests=OFF
+run_longhaul_tests=OFF
+build_amqp=ON
+build_http=ON
+build_mqtt=ON
+no_blob=OFF
+run_unittests=OFF
+build_python=OFF
+build_javawrapper=OFF
+run_valgrind=0
+build_folder=$(pwd)
+make=true
+toolchainfile="-DCMAKE_TOOLCHAIN_FILE=${script_dir}/cmake_toolchain_file_armhf_linux"
+cmake_install_prefix=" "
+no_logging=OFF
+wip_use_c2d_amqp_methods=OFF
 
-$(dirname "$0")/build.sh --toolchain-file cmake_toolchain_file_armhf_linux -cl --sysroot=$ARMHF_SYSROOT_DIR -cl -L${ARMHF_SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf
+cmake $toolchainfile $cmake_install_prefix -Drun_valgrind:BOOL=$run_valgrind -DcompileOption_C:STRING="$extracloptions" -Drun_e2e_tests:BOOL=$run_e2e_tests -Drun_longhaul_tests=$run_longhaul_tests -Duse_amqp:BOOL=$build_amqp -Duse_http:BOOL=$build_http -Duse_mqtt:BOOL=$build_mqtt -Ddont_use_uploadtoblob:BOOL=$no_blob -Drun_unittests:BOOL=$run_unittests -Dbuild_python:STRING=$build_python -Dbuild_javawrapper:BOOL=$build_javawrapper -Dno_logging:BOOL=$no_logging $build_root -Dwip_use_c2d_amqp_methods:BOOL=$wip_use_c2d_amqp_methods
