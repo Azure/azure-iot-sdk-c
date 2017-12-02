eae01cb4b61fcb88725624be6014d97468eebb19
John Spaith <jspaith@windows.microsoft.com>
2017-12-01 16:56:23 -0800

    Use %d to avoid clang/valgrind complaint

--- iothub_client/src/blob.c
+++ iothub_client/src/blob.c
@@ -232,7 +232,7 @@ BLOB_RESULT Blob_UploadMultipleBlocksFromSasUri(const char* SASURI, IOTHUB_CLIEN
                                     if (size > BLOCK_SIZE)
                                     {
                                         /*Codes_SRS_BLOB_99_001: [ If the size of the block returned by `getDataCallback` is bigger than 4MB, then `Blob_UploadMultipleBlocksFromSasUri` shall fail and return `BLOB_INVALID_ARG`. ]*/
-                                        LogError("tried to upload block of size %zu, max allowed size is %zu", size, BLOCK_SIZE);
+                                        LogError("tried to upload block of size %zu, max allowed size is %d", size, BLOCK_SIZE);
                                         result = BLOB_INVALID_ARG;
                                         isError = 1;
                                     }
@@ -286,7 +286,7 @@ BLOB_RESULT Blob_UploadMultipleBlocksFromSasUri(const char* SASURI, IOTHUB_CLIEN
                                                 if (size > BLOCK_SIZE)
                                                 {
                                                     /*Codes_SRS_BLOB_99_001: [ If the size of the block returned by `getDataCallback` is bigger than 4MB, then `Blob_UploadMultipleBlocksFromSasUri` shall fail and return `BLOB_INVALID_ARG`. ]*/
-                                                    LogError("tried to upload block of size %zu, max allowed size is %zu", size, BLOCK_SIZE);
+                                                    LogError("tried to upload block of size %zu, max allowed size is %d", size, BLOCK_SIZE);
                                                     result = BLOB_INVALID_ARG;
                                                     isError = 1;
                                                 }
