15b1e6ca21f9a29fd19fd5b4eb5c283301707647
Roy Sprowl <roys@jetstreamsoftware.com>
2018-02-26 12:18:29 -0800

    Use XCode for Apple builds

--- jenkins/osx_c.sh
+++ jenkins/osx_c.sh
@@ -14,7 +14,7 @@ CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 rm -r -f $build_folder
 mkdir -p $build_folder
 pushd $build_folder
-cmake .. -DOPENSSL_ROOT_DIR:PATH=/usr/local/opt/openssl -Drun_unittests:bool=ON
+cmake .. -DOPENSSL_ROOT_DIR:PATH=/usr/local/opt/openssl -Drun_unittests:bool=ON -G Xcode
 cmake --build . -- --jobs=$CORES
 ctest -C "debug" -V
 popd
