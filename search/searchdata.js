0e4c46e404fee7eab7965d2354e3cd453f716c75
John Spaith <jspaith@windows.microsoft.com>
2018-03-07 08:09:00 -0800

    release_2018_03_02

--- search/searchdata.js
+++ search/searchdata.js
@@ -7,7 +7,8 @@ var indexSectionsWithContent =
   4: "dipt",
   5: "hilst",
   6: "hilst",
-  7: "bdegisw"
+  7: "bdegisw",
+  8: "m"
 };
 
 var indexSectionNames =
@@ -19,7 +20,8 @@ var indexSectionNames =
   4: "variables",
   5: "typedefs",
   6: "enums",
-  7: "defines"
+  7: "defines",
+  8: "pages"
 };
 
 var indexSectionLabels =
@@ -31,6 +33,7 @@ var indexSectionLabels =
   4: "Variables",
   5: "Typedefs",
   6: "Enumerations",
-  7: "Macros"
+  7: "Macros",
+  8: "Pages"
 };
 
