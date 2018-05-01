7b5f36883f1d5d5af3a0d269110699bb00ee94e5
Mark Huang <mark@tignis.com>
2018-04-30 17:33:38 -0700

    Fix #83: Python 3.6 support when building SDK
    Fix issue when --build-python 3.6 is specified:
    
    ./c/build_all/linux/build.sh: line 77: [3.6: command not found
    ./c/build_all/linux/build.sh: line 77: [3.6: command not found
    
    Bash conditionals require spaces between the brackets.

--- build_all/linux/build.sh
+++ build_all/linux/build.sh
@@ -74,7 +74,7 @@ process_args ()
       then
         # save the arg to python version
         build_python="$arg"
-        if [ $build_python != "2.7" ] && [ $build_python != "3.4" ] && [ $build_python != "3.5" ] && [$build_python != "3.6"]
+        if [ $build_python != "2.7" ] && [ $build_python != "3.4" ] && [ $build_python != "3.5" ] && [ $build_python != "3.6" ]
         then
           echo "Supported python versions are 2.7, 3.4 or 3.5 or 3.6"
           exit 1
