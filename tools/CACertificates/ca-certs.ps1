6ebd5a00a3d6035c1b419ba329b0368d154f1f47
Mahesh Rohera <marohera@microsoft.com>
2018-07-10 14:14:50 -0700

    WIP

--- tools/CACertificates/ca-certs.ps1
+++ tools/CACertificates/ca-certs.ps1
@@ -10,51 +10,25 @@ Set-StrictMode -Version 2.0
 #
 
 # Errors in system routines will stop script execution
-$errorActionPreference    = "stop"
+$errorActionPreference       = "stop"
 
 $_basePath                   = "."
 $_rootCertCommonName         = "Azure IoT CA TestOnly Root CA"
 $_rootCertSubject            = "`"/CN=$_rootCertCommonName`""
+$_rootCAPrefix               = "azure-iot-test-only.root.ca"
 $_intermediateCertCommonName = "Azure IoT CA TestOnly Intermediate {0} CA"
-#todo remove
-$_intermediateCertSubject    = "`"/CN=$_intermediateCertCommonName`""
+$_intermediate1CommonName    = ($_intermediateCertCommonName -f "1")
 $_intermediate1Prefix        = "azure-iot-test-only.intermediate1"
 $_privateKeyPassword         = "1234"
 $_keyBitsLength              = "4096"
 $_days_until_expiration      = 30
 $_opensslRootConfigFile      = "$_basePath/openssl_root_ca.cnf"
 $_opensslIntermediateConfigFile = "$_basePath/openssl_device_intermediate_ca.cnf"
-
-$rootCAPrefix                = "azure-iot-test-only.root.ca"
-$keySuffix                   = "key.pem"
-$certSuffix                  = "cert.pem"
-$csrSuffix                   = "csr.pem"
-
-#todo remove
-$rootCAPemFileName          = "./RootCA.pem"
-$intermediate1CAPemFileName = "./Intermediate1.pem"
-
-# Variables containing file paths for Edge certificates
-$edgeDeviceCertificate      = "$_basePath/certs/new-edge-device.cert.pem"
-$edgeDevicePrivateKey       = "$_basePath/private/new-edge-device.key.pem"
-$edgeDeviceFullCertChain    = "$_basePath/certs/new-edge-device-full-chain.cert.pem"
-
+$_keySuffix                  = "key.pem"
+$_certSuffix                 = "cert.pem"
+$_csrSuffix                  = "csr.pem"
 # Whether to use ECC or RSA is stored in a file.  If it doesn't exist, we default to ECC.
-$algorithmUsedFile       = "./algorithmUsed.txt"
-
-<#
-    .SYNOPSIS
-        Prints a warning message noting that the certificates generated are not for production
-        environments.
-    .DESCRIPTION
-        Prints a warning message noting that the certificates generated are not for production
-        environments.
-#>
-function Print-WarningCertsNotForProduction()
-{
-    Write-Warning "This script is provided for prototyping only."
-    Write-Warning "DO NOT USE CERTIFICATES FROM THIS SCRIPT FOR PRODUCTION!"
-}
+$algorithmUsedFile           = "./algorithmUsed.txt"
 
 <#
     .SYNOPSIS
@@ -103,7 +77,7 @@ function Invoke-External()
 #>
 function Get-CertPathForPrefix([string]$prefix)
 {
-    return "$_basePath/certs/$prefix.$certSuffix"
+    return "$_basePath/certs/$prefix.$_certSuffix"
 }
 
 <#
@@ -115,7 +89,7 @@ function Get-CertPathForPrefix([string]$prefix)
 #>
 function Get-KeyPathForPrefix([string]$prefix)
 {
-    return "$_basePath/private/$prefix.$keySuffix"
+    return "$_basePath/private/$prefix.$_keySuffix"
 }
 
 <#
@@ -128,11 +102,19 @@ function Get-KeyPathForPrefix([string]$prefix)
 #>
 function Get-CSRPathForPrefix([string]$prefix)
 {
-    return "$_basePath/csr/$prefix.$csrSuffix"
+    return "$_basePath/csr/$prefix.$_csrSuffix"
 }
 
-# The script puts certs into the global certificate store. If there is already a cert of the
-# same name present, we're not going to be able to tell the new apart from the old, so error out.
+<#
+    .SYNOPSIS
+    Function to check if there is already a cert of the root CA present in the certificate store.
+    .DESCRIPTION
+    This function detects if a the root CA certifcate is already installed in the cert store
+    and warns the user to clean it up since they might be installing newer ones after
+    executing this script.
+    .PARAMETER printMsg
+    An optional bool that controls if additional diagnotics messages are to be printed or not.
+#>
 function Test-CACertNotInstalledAlready([bool]$printMsg=$true)
 {
     if ($TRUE -eq $printMsg)
@@ -169,8 +151,14 @@ function Test-CACertNotInstalledAlready([bool]$printMsg=$true)
     }
 }
 
+
 <#
-    Verify that the prerequisites for this script are met
+    .SYNOPSIS
+    Verify that the prerequisites for this script are met.
+    .DESCRIPTION
+    Helper function verify that the prerequisites dependencies for this script are met
+    .PARAMETER printMsg
+        An optional bool that controls if additional diagnotics messages are to be printed or not.
 #>
 function Test-CACertsPrerequisites([bool]$printMsg=$true)
 {
@@ -190,6 +178,13 @@ function Test-CACertsPrerequisites([bool]$printMsg=$true)
     }
 }
 
+<#
+    .SYNOPSIS
+    Prepare the nessary files and directories to hold the resulting certificates.
+    .DESCRIPTION
+    Prepare the nessary files and directories to hold the resulting certificates. This will be
+    called if a new Root CA is being created thus wiping out any prior generated certificates.
+#>
 function PrepareFilesystem()
 {
     Remove-Item -Path $_basePath/csr -Recurse
@@ -212,6 +207,18 @@ function PrepareFilesystem()
     "1000`n" | Out-File -NoNewline -Encoding ASCII $_basePath/serial
 }
 
+<#
+    .SYNOPSIS
+    Creates an asymmetric private key to be used for certificate generation.
+    .DESCRIPTION
+    Generate an ECC or RSA key with an optional passphrase. The choice of what type of key
+    to create is determined by function Get-CACertsCertUseRSA(). The private key file
+    will be saved to the private dir and named using the provided prefix.
+    .PARAMETER prefix
+        Prefix is used to name the file containing the private key
+    .PARAMETER keyPass
+        Optional passphrase used to encrypt the private key
+#>
 function New-PrivateKey([string]$prefix, [string]$keyPass=$NULL)
 {
     Write-Host ("Creating the $prefix private Key")
@@ -223,25 +230,52 @@ function New-PrivateKey([string]$prefix, [string]$keyPass=$NULL)
     }
 
     $algorithm = ""
+    $cmdEpilog = ""
     if ((Get-CACertsCertUseRSA) -eq $TRUE)
     {
-        $algorithm = "genrsa $_keyBitsLength"
+        $algorithm = "genrsa"
+        $cmdEpilog = "$_keyBitsLength"
     }
     else
     {
         $algorithm = "ecparam -genkey -name secp256k1"
         $passwordCreateCmd = " | openssl ec $passwordCreateCmd"
+        $cmdEpilog = ""
     }
 
     $keyFile = Get-KeyPathForPrefix($prefix)
-    $cmd = "openssl $algorithm $passwordCreateCmd -out $keyFile"
+    $cmd = "openssl $algorithm $passwordCreateCmd -out $keyFile $cmdEpilog"
     Invoke-External -verbose $cmd
     return $keyFile
 }
 
+<#
+    .SYNOPSIS
+    Generate a certificate using an issuer certificate and key. Certificates could be
+    either CA, server and client certificates.
+    .DESCRIPTION
+    Generate a client certificate using the supplied certificate and key parameters and
+    have it signed by an issuer certificate and key.
+    Issuer is spcified by its prefix and its corresponding key and certificate
+    will be retrieved from the filesystem.
+    .PARAMETER x509Ext
+        A string to identify which configuration to use to generate a certificate.
+    .PARAMETER expirationDays
+        Number of days from now to set the certificate expiration timestamp.
+    .PARAMETER subject
+        Value of the certificate subject to use to generate a certificate.
+    .PARAMETER prefix
+        Prefix is used to name the file containing the private key
+    .PARAMETER issuerPrefix
+        Issuer prefix to look up the certifcate and key
+    .PARAMETER keyPass
+        Optional passphrase used to encrypt the private key
+    .PARAMETER issuerKeyPass
+        Optional passphrase used to decrypt the issuer private key when signing the certificate.
+#>
 function New-IntermediateCertificate
 (
-    [string]$x509Ext,
+    [ValidateSet("usr_cert","v3_intermediate_ca","server_cert")][string]$x509Ext,
     [string]$expirationDays,
     [string]$subject,
     [string]$prefix,
@@ -302,7 +336,7 @@ function New-IntermediateCertificate
 
     Write-Host ("Verifying the certificate for $prefix with issuer certificate $issuerPrefix")
     Write-Host "-----------------------------------"
-    $rootCertFile = Get-CertPathForPrefix($rootCAPrefix)
+    $rootCertFile = Get-CertPathForPrefix($_rootCAPrefix)
     $cmd =  "openssl verify -CAfile $rootCertFile -untrusted $issuerCertFile $certFile"
     Invoke-External -verbose $cmd
 
@@ -315,6 +349,20 @@ function New-IntermediateCertificate
     return $certFile
 }
 
+<#
+    .SYNOPSIS
+    Generate a client certificate using an issuer certificate and key
+    .DESCRIPTION
+    Generate a client certificate using the supplied common name and have it signed by
+    an issuer certificate and key. Issuer is spcified by its prefix and its corresponding
+    key and certificate will be retrieved from the filesystem.
+    .PARAMETER prefix
+        Prefix is used to name the file containing the private key
+    .PARAMETER issuerPrefix
+        Issuer prefix to look up the certifcate and key
+    .PARAMETER commonName
+        Value of the CN field to set when generating the certifcate
+#>
 function New-ClientCertificate([string]$prefix, [string]$issuerPrefix, [string]$commonName)
 {
     $subject = "`"/CN=$commonName`""
@@ -323,6 +371,20 @@ function New-ClientCertificate([string]$prefix, [string]$issuerPrefix, [string]$
     return $certFile
 }
 
+<#
+    .SYNOPSIS
+    Generate a server certificate using an issuer certificate and key
+    .DESCRIPTION
+    Generate a server certificate using the supplied common name and have it signed by
+    an issuer certificate and key. Issuer is spcified by its prefix and its corresponding
+    key and certificate will be retrieved from the filesystem.
+    .PARAMETER prefix
+        Prefix is used to name the file containing the private key
+    .PARAMETER issuerPrefix
+        Issuer prefix to look up the certifcate and key
+    .PARAMETER commonName
+        Value of the CN field to set when generating the certifcate
+#>
 function New-ServerCertificate([string]$prefix, [string]$issuerPrefix, [string]$commonName)
 {
     $subject = "`"/CN=$commonName`""
@@ -331,6 +393,20 @@ function New-ServerCertificate([string]$prefix, [string]$issuerPrefix, [string]$
     return $certFile
 }
 
+<#
+    .SYNOPSIS
+    Generate an intermediate CA certificate using an issuer certificate and key
+    .DESCRIPTION
+    Generate an intermediate CA certificate using the supplied common name and have it signed by
+    an issuer certificate and key. Issuer is spcified by its prefix and its corresponding
+    key and certificate will be retrieved from the filesystem.
+    .PARAMETER prefix
+        Prefix is used to name the file containing the private key
+    .PARAMETER issuerPrefix
+        Issuer prefix to look up the certifcate and key
+    .PARAMETER commonName
+        Value of the CN field to set when generating the certifcate
+#>
 function New-IntermediateCACertificate([string]$prefix, [string]$issuerPrefix, [string]$commonName)
 {
     $subject = "`"/CN=$commonName`""
@@ -339,11 +415,17 @@ function New-IntermediateCACertificate([string]$prefix, [string]$issuerPrefix, [
     return $certFile
 }
 
+<#
+    .SYNOPSIS
+    Generate a root CA certificate
+    .DESCRIPTION
+    Generate a root CA certificate using the parameters specified in the script.
+#>
 function New-RootCACertificate()
 {
     Write-Host ("Creating the Root CA private key")
-    $keyFile = New-PrivateKey $rootCAPrefix $_privateKeyPassword
-    $certFile = Get-CertPathForPrefix($rootCAPrefix)
+    $keyFile = New-PrivateKey $_rootCAPrefix $_privateKeyPassword
+    $certFile = Get-CertPathForPrefix($_rootCAPrefix)
 
     Write-Host ("Creating the Root CA certificate")
     $passwordUseCmd = "-passin pass:$_privateKeyPassword"
@@ -375,7 +457,7 @@ function New-CACertsCertChain([Parameter(Mandatory=$TRUE)][ValidateSet("rsa","ec
     Set-Content $algorithmUsedFile $algorithm
 
     New-RootCACertificate
-    New-IntermediateCACertificate $_intermediate1Prefix $rootCAPrefix ($_intermediateCertCommonName -f "1")
+    New-IntermediateCACertificate $_intermediate1Prefix $_rootCAPrefix $_intermediate1CommonName
     Write-Host "Success"
 }
 
@@ -385,6 +467,16 @@ function Get-CACertsCertUseRsa()
     Write-Output ((Get-Content $algorithmUsedFile -ErrorAction SilentlyContinue) -eq "rsa")
 }
 
+<#
+    .SYNOPSIS
+    Queries the Windows certificate store to check if a certificate is already installed using the
+    provided subjectName.
+    .DESCRIPTION
+    Queries the Windows certificate store to check if a certificate is already installed using the
+    provided subjectName.
+    .PARAMETER subjectName
+        Subject name to be used when querying the certificate store.
+#>
 function Get-CACertsCertBySubjectName([string]$subjectName)
 {
     $certificates = gci -Recurse Cert:\LocalMachine\ |? { $_.gettype().name -eq "X509Certificate2" }
@@ -397,7 +489,16 @@ function Get-CACertsCertBySubjectName([string]$subjectName)
     Write-Output $cert
 }
 
-function New-CACertsVerificationCert([string]$requestedCommonName)
+<#
+    .SYNOPSIS
+    Generate a client certificate to be used for private key possession test for IoT Hub
+    .DESCRIPTION
+    The verification certificate is client certificate issued by the root CA. The IoT hub
+    generated a common name which is to be supplied as parameter requestedCommonName.
+    .PARAMETER requestedCommonName
+        Common name to be used when generating the IoT Hub certificate.
+#>
+function New-CACertsVerificationCert([Parameter(Mandatory=$TRUE)][string]$requestedCommonName)
 {
     if ([string]::IsNullOrWhiteSpace($requestedCommonName))
     {
@@ -407,9 +508,9 @@ function New-CACertsVerificationCert([string]$requestedCommonName)
     $requestedCommonName = $requestedCommonName.Trim()
     $verifCertPath = Get-CertPathForPrefix($verificationPrefix)
     $verifKeyPath = Get-KeyPathForPrefix($verificationPrefix)
-    Remove-Item -Path $verifCertPath
-    Remove-Item -Path $verifKeyPath
-    New-ClientCertificate $verificationPrefix $rootCAPrefix $requestedCommonName
+    Remove-Item -Path $verifCertPath -ErrorAction SilentlyContinue
+    Remove-Item -Path $verifKeyPath -ErrorAction SilentlyContinue
+    New-ClientCertificate $verificationPrefix $_rootCAPrefix $requestedCommonName
     $verifyRequestedFileName = ".\$verificationPrefix.cer"
     if (-not (Test-Path $verifCertPath))
     {
@@ -423,7 +524,23 @@ function New-CACertsVerificationCert([string]$requestedCommonName)
     Write-Host ("Certificate with subject CN={0} has been output to {1}" -f $requestedCommonName, (Join-Path (get-location).path $verifyRequestedFileName))
 }
 
-function New-CACertsDevice([string]$deviceName, [string]$signingCertPrefix=$rootCAPrefix, [bool]$isEdgeDevice=$false)
+<#
+    .SYNOPSIS
+    Generate an IoT Edge/Hub device certificate using an issuer's certificate and key
+    .DESCRIPTION
+    Generate a certificate using the supplied common name and have it signed by
+    an issuer certificate and key. Issuer is spcified by its prefix and its corresponding
+    key and certificate will be retrieved from the filesystem.
+    If parameter isEdgeDevice is true the resulting certificate is a CA certificate else a
+    client certificate will be created.
+    .PARAMETER deviceName
+        Device name will be used as a prefix and as the value of the CN field
+    .PARAMETER issuerPrefix
+        Optional issuer prefix to look up the certifcate and key. If null the root CA is used.
+    .PARAMETER isEdgeDevice
+        Identifies if this certificate is to be used for Edge runtime. Default is false.
+#>
+function New-CACertsDevice([Parameter(Mandatory=$TRUE)][string]$deviceName, [string]$issuerPrefix=$_rootCAPrefix, [bool]$isEdgeDevice=$false)
 {
     if ([string]::IsNullOrWhiteSpace($deviceName))
     {
@@ -434,37 +551,45 @@ function New-CACertsDevice([string]$deviceName, [string]$signingCertPrefix=$root
     # Certificates for edge devices need to be able to sign other certs.
     if ($isEdgeDevice -eq $true)
     {
-        $certFile = New-IntermediateCACertificate $deviceName $signingCertPrefix $deviceName
+        $certFile = New-IntermediateCACertificate $deviceName $issuerPrefix $deviceName
     }
     else
     {
-        $certFile = New-ClientCertificate $deviceName $signingCertPrefix $deviceName
+        $certFile = New-ClientCertificate $deviceName $issuerPrefix $deviceName
     }
 
     Write-Host ("Certificate with subject CN={0} has been output to {1}" -f $deviceName, $certFile)
     return $certFile
 }
 
-function New-CACertsEdgeDevice([string]$deviceName, [string]$signingCertPrefix=$_intermediate1Prefix)
+<#
+    .SYNOPSIS
+    Generate an IoT Edge device CA certificate, private key and full certificate chain
+    using an issuer's certificate and key
+    .DESCRIPTION
+    Generate a certificate using the supplied common name and have it signed by
+    an issuer certificate and key. Issuer is spcified by its prefix and its corresponding
+    key and certificate will be retrieved from the filesystem.
+    .PARAMETER deviceName
+        Device name will be used as a prefix and as the value of the CN field
+    .PARAMETER issuerPrefix
+        Optional issuer prefix to look up the certifcate and key. If null the default intermediate
+        CA certificate is used.
+#>
+function New-CACertsEdgeDevice([Parameter(Mandatory=$TRUE)][string]$deviceName, [string]$issuerPrefix=$_intermediate1Prefix)
 {
     if ([string]::IsNullOrWhiteSpace($deviceName))
     {
         throw "Edge device name string parameter is required and cannot be null or empty"
     }
     $deviceName = $deviceName.Trim()
-    $edgeDeviceCertificate = New-CACertsDevice $deviceName $signingCertPrefix $true
+    $edgeDeviceCertificate = New-CACertsDevice $deviceName $issuerPrefix $true
     $intermediate1CertFileName  = Get-CertPathForPrefix($_intermediate1Prefix)
-    $rootCACertFileName  = Get-CertPathForPrefix($rootCAPrefix)
+    $rootCACertFileName  = Get-CertPathForPrefix($_rootCAPrefix)
     $edgeDeviceFullCertChain = Get-CertPathForPrefix("$deviceName-full-chain")
 
-    Get-Content $edgeDeviceCertificate, $intermediate1CAPemFileName, $rootCACertFileName | Set-Content $edgeDeviceFullCertChain
+    Get-Content $edgeDeviceCertificate, $intermediate1CertFileName, $rootCACertFileName | Set-Content $edgeDeviceFullCertChain
 }
 
-# Outputs certificates for Edge device using naming conventions from tutorials
-function Write-CACertsCertificatesForEdgeDevice([string]$deviceName)
-{
-    Write-Warning("This command no longer needs to be run")
-    Write-Host "Success"
-}
-
-Print-WarningCertsNotForProduction
+Write-Warning "This script is provided for prototyping only."
+Write-Warning "DO NOT USE CERTIFICATES FROM THIS SCRIPT FOR PRODUCTION!"
