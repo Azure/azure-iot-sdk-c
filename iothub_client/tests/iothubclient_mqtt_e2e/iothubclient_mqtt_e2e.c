f43e77b462f24c2714497033924fc55dba32a460
John Spaith <jspaith@windows.microsoft.com>
2018-08-21 16:05:50 +0000

    Re-enable e2e tests (test code only, no product changes)

--- iothub_client/tests/iothubclient_mqtt_e2e/iothubclient_mqtt_e2e.c
+++ iothub_client/tests/iothubclient_mqtt_e2e/iothubclient_mqtt_e2e.c
@@ -61,7 +61,7 @@ TEST_FUNCTION(IoTHub_MQTT_RecvMessage_E2ETest_sas_urlDecode)
 }
 
 #ifndef __APPLE__
-/*TEST_FUNCTION(IoTHub_MQTT_SendEvent_e2e_x509)
+TEST_FUNCTION(IoTHub_MQTT_SendEvent_e2e_x509)
 {
 #ifdef AZIOT_LINUX
     g_e2e_test_options.set_mac_address = false;
@@ -69,13 +69,15 @@ TEST_FUNCTION(IoTHub_MQTT_RecvMessage_E2ETest_sas_urlDecode)
     e2e_send_event_test_x509(MQTT_Protocol);
 }
 
+
 TEST_FUNCTION(IoTHub_MQTT_RecvMessage_E2ETest_x509)
 {
 #ifdef AZIOT_LINUX
     g_e2e_test_options.set_mac_address = true;
 #endif
     e2e_recv_message_test_x509(MQTT_Protocol);
-}*/
+}
 #endif
+
 
 END_TEST_SUITE(iothubclient_mqtt_e2e)
