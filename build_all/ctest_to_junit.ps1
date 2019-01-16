6d8bc60a0a3356dd3ffe58892fcfc8ab7f1bb6ee
yodama <yoseph.maguire@microsoft.com>
2019-01-15 16:00:12 -0800

    trying fixes

---
+++ build_all/ctest_to_junit.ps1
@@ -0,0 +1,10 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+# This script will be running on working dir set as build_all  
+$xsl = "./build_all/ctest_to_junit.xsl" 
+$ctest_xml = (Get-ChildItem "./Testing/*/Test.xml").FullName 
+$junit_xml = "./Testing/results-junit.xml" 
+$xslt = New-Object System.Xml.Xsl.XslCompiledTransform 
+$xslt.Load($xsl) 
+$xslt.Transform($ctest_xml, $junit_xml) 
