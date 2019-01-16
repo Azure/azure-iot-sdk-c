64cd978627f461256acd837aeb9bf6ae96c31be5
yodama <yoseph.maguire@microsoft.com>
2019-01-15 16:58:17 -0800

    fix xsl path

--- build_all/ctest_to_junit.ps1
+++ build_all/ctest_to_junit.ps1
@@ -2,7 +2,7 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 # This script will be running on working dir set as build_all  
-$xsl = "./build_all/ctest_to_junit.xsl" 
+$xsl = "./ctest_to_junit.xsl" 
 $ctest_xml = (Get-ChildItem "./Testing/*/Test.xml").FullName 
 $junit_xml = "./Testing/results-junit.xml" 
 $xslt = New-Object System.Xml.Xsl.XslCompiledTransform 
