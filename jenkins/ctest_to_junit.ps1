d107ff580eb7a176218a2e38f5b83dbe966d2ac6
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-01-24 15:32:32 -0800

    Update ctest ps1

--- jenkins/ctest_to_junit.ps1
+++ jenkins/ctest_to_junit.ps1
@@ -4,7 +4,7 @@
 Set-PSDebug -Trace 2
 # This script will be running on working dir set as build_all  
 $xsl = "./jenkins/ctest_to_junit.xsl" 
-$ctest_xml = (Get-ChildItem "*/Test.xml").FullName 
+$ctest_xml = (Get-ChildItem "*/Test.xml" -Recurse).FullName 
 $junit_xml = "./Testing/results-junit.xml" 
 $xslt = New-Object System.Xml.Xsl.XslCompiledTransform 
 $xslt.Load($xsl) 
