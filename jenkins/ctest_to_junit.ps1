962513107d34cba713fb3c7f84633e2a449eb102
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-01-25 17:11:40 -0800

    Fixing Path Issues

--- jenkins/ctest_to_junit.ps1
+++ jenkins/ctest_to_junit.ps1
@@ -3,9 +3,14 @@
 
 Set-PSDebug -Trace 2
 # This script will be running on working dir set as build_all  
-$xsl = "./jenkins/ctest_to_junit.xsl" 
-$ctest_xml = (Get-ChildItem "*/Test.xml" -Recurse).FullName 
-$junit_xml = "./Testing/results-junit.xml" 
+$PSScriptRoot
+Push-Location $PSScriptRoot
+$outfile = Join-Path -Path (Get-Location) -ChildPath "results-junit.xml"
+$xsl = Join-Path -Path (Get-Location) -ChildPath "..\jenkins\ctest_to_junit.xsl" 
+$ctest_xml = (Get-ChildItem "../cmake/Testing/*/Test.xml").FullName
+$junit_xml = Join-Path -Path (Get-Location) -ChildPath "results-junit.xml" 
 $xslt = New-Object System.Xml.Xsl.XslCompiledTransform 
 $xslt.Load($xsl) 
 $xslt.Transform($ctest_xml, $junit_xml)
+
+Pop-Location
