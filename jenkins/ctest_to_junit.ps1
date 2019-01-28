08140450395e034922f3cf7b84295b8fd312fc24
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-01-28 11:27:57 -0800

    fixing scripts

--- jenkins/ctest_to_junit.ps1
+++ jenkins/ctest_to_junit.ps1
@@ -3,14 +3,15 @@
 
 Set-PSDebug -Trace 2
 # This script will be running on working dir set as build_all  
+
+$build_folder = $args[0]
 $PSScriptRoot
 Push-Location $PSScriptRoot
-$outfile = Join-Path -Path (Get-Location) -ChildPath "results-junit.xml"
 $xsl = Join-Path -Path (Get-Location) -ChildPath "..\jenkins\ctest_to_junit.xsl" 
-$ctest_xml = (Get-ChildItem "../cmake/$args[0]/*/Test.xml" -Recurse).FullName
+$ctest_xml = (Get-ChildItem "../cmake/$build_folder/*/Test.xml" -Recurse).FullName
 $junit_xml = Join-Path -Path (Get-Location) -ChildPath "results-junit.xml" 
 $xslt = New-Object System.Xml.Xsl.XslCompiledTransform 
-$xslt.Load($xsl) 
+$xslt.Load($xsl)
 $xslt.Transform($ctest_xml, $junit_xml)
 
 Pop-Location
