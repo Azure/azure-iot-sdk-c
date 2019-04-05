ed392c14d3c6e25eeac39dc6e3e4176304a3605b
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-04-05 08:43:07 -0700

    adding more prints

--- configs/azure_iot_sdksConfig.cmake
+++ configs/azure_iot_sdksConfig.cmake
@@ -1,6 +1,7 @@
 #Copyright (c) Microsoft. All rights reserved.
 #Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+message("azure_iot_sdksConfig.cmake: Line 4: ${CMAKE_CURRENT_LIST_DIR}")
 include("${CMAKE_CURRENT_LIST_DIR}/azure_iot_sdksTargets.cmake")
 
 get_target_property(IOTHUB_CLIENT_INCLUDES iothub_client INTERFACE_INCLUDE_DIRECTORIES)
