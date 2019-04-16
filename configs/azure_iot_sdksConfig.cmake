70c4cdca042cc31b09ef2fb2a8ea466ad1d019be
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-04-15 21:04:58 -0700

    big changes!

--- configs/azure_iot_sdksConfig.cmake
+++ configs/azure_iot_sdksConfig.cmake
@@ -1,13 +1,18 @@
 #Copyright (c) Microsoft. All rights reserved.
 #Licensed under the MIT license. See LICENSE file in the project root for full license information.
+if(${use_installed_dependencies})
 
-message("azure_iot_sdksConfig.cmake: Line 4: ${CMAKE_CURRENT_LIST_DIR}")
+    message("azure_iot_sdksConfig.cmake: Line 4: ${CMAKE_CURRENT_LIST_DIR}")
 
-include(CMakeFindDependencyMacro)
-find_dependency(unofficial-parson)
-find_dependency(uamqp)
-find_dependency(umqtt)
-find_dependency(azure_c_shared_utility)
+    include(CMakeFindDependencyMacro)
+    find_dependency(unofficial-parson)
+    find_dependency(uamqp)
+    find_dependency(umqtt)
+    find_dependency(azure_c_shared_utility)
+    find_library(UHTTP_LIBRARY uhttp)
+    add_library(uhttp STATIC IMPORTED)
+    set_target_properties(uhttp PROPERTIES IMPORTED_LOCATION ${UHTTP_LIBRARY})
+endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/azure_iot_sdksTargets.cmake")
 
