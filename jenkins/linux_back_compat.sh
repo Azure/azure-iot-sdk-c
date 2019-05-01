8c6165df56e987c1cf26f07812dc83ab5125835a
jebrando <jelani.brandon@microsoft.com>
2019-05-01 09:31:35 -0700

    Fixing the linux back compat script

--- jenkins/linux_back_compat.sh
+++ jenkins/linux_back_compat.sh
@@ -8,42 +8,37 @@ set -e
 cat /etc/*release | grep VERSION*
 gcc --version
 
-# back out of jenkins folder
-# echo "this is where we start "$(pwd)
-# cd ..
-# save sdk root
-sdk_root=$(pwd)
-echo $sdk_root && ls
-
-cd ..
-git clone https://github.com/Azure/azure-iot-c-back-compat.git --recursive
-
-back_compat_folder=$(cd "$sdk_root"/../azure-iot-c-back-compat/"
-ls
-build_root="$sdk_root"
-cd $build_root
-echo "sdk build root $build_root"
-
-build_folder=$build_root"/cmake/sdk"
-
 # Set the default cores
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 
-rm -rf $build_folder
-mkdir -p $build_folder
-pushd $build_folder
+build_root=$(cd "$(dirname "$0")/.." && pwd)
+cd $build_root
+
+echo "Build Root $build_root"
+
+sdk_build_folder=$build_root"/cmake"
+
+# Build the SDK
+rm -rf $sdk_build_folder
+mkdir -p $sdk_build_folder
+pushd $sdk_build_folder
 cmake $build_root -Drun_e2e_tests=ON
 make install --jobs=$CORES
 popd
 
-# Now run back compat
-cd $sdk_root
-cd "/../azure-iot-c-back-compat"
-mkdir -p "cmake"
-pushd $back_compat_folder
+cd "$build_root/.."
 
-# By default back compat uses the installed components
-cmake "$sdk_root/../azure-iot-c-back-compat"
+echo "Clone the back compat repo into " pwd
+git clone https://github.com/Azure/azure-iot-c-back-compat.git --recursive
+
+back_compat_root="$build_root/azure-iot-c-back-compat"
+back_compat_build=$back_compat_root"/cmake"
+
+# Now run back compat
+rm -rf $back_compat_build
+mkdir -p $back_compat_build
+pushd $back_compat_build
+cmake $back_compat_root
 make --jobs=$CORES
 
 ctest -j $CORES --output-on-failure
