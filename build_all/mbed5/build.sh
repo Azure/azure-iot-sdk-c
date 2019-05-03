695f7de0b5ae4985d188b886dd03208a50038985
jbobotek <jeremy.bobotek@gmail.com>
2019-05-03 16:30:31 -0700

    Update build.sh

--- build_all/mbed5/build.sh
+++ build_all/mbed5/build.sh
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
+sudo rm -R -f mxchip
 git clone -b rajeev/latest_mbed https://github.com/massand/mxchip_az3166_firmware.git mxchip
 # rsync -avz --existing ./ mxchip/mbed-iot-devkit-sdk/cores/arduino/azure-iot-sdk-c/
 
@@ -12,6 +13,7 @@ sudo git checkout -- mbed-iot-devkit-sdk/cores/arduino/azure-iot-sdk-c/provision
 sudo git checkout -- mbed-iot-devkit-sdk/cores/arduino/azure-iot-sdk-c/provisioning_client/deps/RIoT/Reference/RIoT/Core/RIoTCrypt/RiotDerEnc.c
 sudo git checkout -- mbed-iot-devkit-sdk/cores/arduino/azure-iot-sdk-c/provisioning_client/deps/RIoT/Reference/RIoT/Core/RIoTCrypt/RiotEcc.c
 sudo git checkout -- mbed-iot-devkit-sdk/cores/arduino/azure-iot-sdk-c/provisioning_client/deps/RIoT/Reference/RIoT/Core/RIoTCrypt/RiotSha256.c
+
 sudo iotz init mbed
 sudo iotz compile
 
