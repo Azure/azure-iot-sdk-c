2e679959049475a660b5560599b53296bbfbb8f4
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-05-06 18:04:16 -0700

    bugfix

--- jenkins/raspberrypi/fetch.sh
+++ jenkins/raspberrypi/fetch.sh
@@ -10,6 +10,7 @@ if [ -s /${temp}/source.tar.gz ]; then
   [ $? -eq 0 ] || { echo "mkdir /temp/source failed"; exit 1; }
   tar -zxf /${temp}/source.tar.gz -C /${temp}/source 
   [ $? -eq 0 ] || { echo "tar -zxf failed"; exit 1; }
+  ls -al /${temp}/source
   rsync --recursive --checksum --update --delete-during  /${temp}/source/ .
   [ $? -eq 0 ] || { echo "rsync failed"; exit 1; }
 else
