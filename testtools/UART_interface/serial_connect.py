73c3222258e1021e7372988515ef99f87fac402e
jbobotek <jeremybobotek@gmail.com>
2019-05-16 16:40:03 -0700

    Added usage dictionary file to print out serial_connect.py usage

--- testtools/UART_interface/serial_connect.py
+++ testtools/UART_interface/serial_connect.py
@@ -5,13 +5,23 @@ import getopt
 import time
 try:
     import testtools.UART_interface.serial_settings as serial_settings
+    import testtools.UART_interface.serial_commands_dict as commands_dict
 except:
     import serial_settings
+    import serial_commands_dict as commands_dict
 
 
 # Note: commands on MXCHIP have line endings with \r AND \n
 # Oz request: Make this multidevice - buh how? Guess we gots ta figure out
 # Notes: This is designed to be used as a command line script with args (for automation purposes) to communicate over serial to a Microsoft mxchip device.
+
+# Iterates through command dictionary to print out script's opt usage
+def usage():
+    usage_txt = "serial_connect.py usage: \r\n"
+    for commands in commands_dict.cmds:
+        usage_txt += " - %s: " %commands + commands_dict.cmds[commands]['text'] + "\r\n"
+
+    return usage_txt
 
 def parse_opts():
     options, remainder = getopt.gnu_getopt(sys.argv[1:], 'hsi:o:b:p:m:', ['input', 'output', 'help', 'skip', 'baudrate', 'port', 'mxchip_file'])
@@ -19,27 +29,22 @@ def parse_opts():
 
     for opt, arg in options:
         if opt in ('-h', '--help'):
-            print('I need somebody')
+            print(usage())
         elif opt in ('-i', '--input'):
-            # print('Setting --file.')
             serial_settings.input_file = arg
         elif opt in ('-o', '--output'):
-            # print('Setting --file.')
             serial_settings.output_file = arg
         elif opt in ('-b', '--baudrate'):
-            # print('Setting --baudrate.')
             serial_settings.baud_rate = int(arg)
         elif opt in ('-p', '--port'):
             serial_settings.port = arg
         elif opt in ('-m', '--mxchip_file'):
             serial_settings.mxchip_file = "/media/newt/" + arg
         elif opt in ('-s', '--skip'):
-            # skip waiting for setup
             serial_settings.skip_setup = True
 
 
 # If there is a sudden disconnect, program should report line in input script reached, and close files.
-
 # method to write to serial line with connection monitoring
 def serial_write(ser, message, file=None):
 
@@ -132,9 +137,6 @@ def write_read(ser, input_file, output_file):
 
 def run():
     if 'posix' in os.name:
-        # enable read write access on port
-        # os.chmod(serial_settings.port, 666)
-
         # check to see that mxchip firmware drop path resolves
         for x in range(serial_settings.wait_for_flash):
             exists = os.path.exists(serial_settings.mxchip_file)
@@ -142,13 +144,12 @@ def run():
                 break
             time.sleep(1)
         if x >= (serial_settings.wait_for_flash-1):
+            # firmware flash path did not resolve
             print("MXCHIP drop path did not resolve, chip did not flash successfully.")
             raise FileExistsError
 
     ser = serial.Serial(port=serial_settings.port, baudrate=serial_settings.baud_rate)
 
-    # print(ser.writable())
-    # print(ser.readable())
     time.sleep(.5)
 
     # Print initial message
@@ -168,7 +169,7 @@ def run():
         # Wait for WiFi and IoT Hub setup to complete
         start_time = time.time()
 
-        while(serial_settings.setup_string not in output) and ((time.time() - start_time) < (3*serial_settings.wait_for_flash)):
+        while(serial_settings.setup_string not in output) and ((time.time() - start_time) < (3*serial_settings.wait_for_flash + 5)):
             time.sleep(.1)
             output = ser.readline(ser.in_waiting)
             output = output.strip().decode(encoding='utf-8')
