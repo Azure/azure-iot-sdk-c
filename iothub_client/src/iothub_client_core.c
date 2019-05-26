4c74035e5e26ee1e7b679a04993aef0cfc1d9c1d
Yoseph Maguire <yoseph.maguire@microsoft.com>
2019-05-26 09:05:41 -0700

    add logging for deprecated option

--- iothub_client/src/iothub_client_core.c
+++ iothub_client/src/iothub_client_core.c
@@ -1733,8 +1733,11 @@ IOTHUB_CLIENT_RESULT IoTHubClientCore_SetOption(IOTHUB_CLIENT_CORE_HANDLE iotHub
                 }
             }
             /* Codes_SRS_IOTHUBCLIENT_41_005: [ If parameter `optionName` is `OPTION_MESSAGE_TIMEOUT` then `IoTHubClientCore_SetOption` shall set `currentMessageTimeout` parameter of `IoTHubClientInstance` ]*/
-            else if (strcmp(OPTION_MESSAGE_TIMEOUT, optionName) == 0)
+            else if (strcmp(OPTION_SERVICE_SIDE_KEEP_ALIVE_FREQ_SECS, optionName) == 0 || strcmp(OPTION_MESSAGE_TIMEOUT, optionName) == 0)
             {
+                if (strcmp(OPTION_MESSAGE_TIMEOUT, optionName) == 0)  {
+                    LogInfo("OPTION_MESSAGE_TIMEOUT is DEPRECATED! Use OPTION_SERVICE_SIDE_KEEP_ALIVE_FREQ_SECS for AMQP; MQTT has no option available. OPTION_MESSAGE_TIMEOUT legacy variable will be kept for back-compat.");
+                }
                 iotHubClientInstance->currentMessageTimeout = * (tickcounter_ms_t *)value;
 
                 /* Codes_SRS_IOTHUBCLIENT_41_004: [ If `currentMessageTimeout` is not greater than `do_work_freq_ms`, `IotHubClientCore_SetOption` shall return `IOTHUB_CLIENT_INVALID_ARG` ]*/
