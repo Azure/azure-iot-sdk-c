d04866cbe5c2f989c8be49d74627737df63d8baf
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2019-06-06 11:05:11 -0700

    Cap the maximum load of make build to 80% of number of CORES

--- jenkins/debian_c.sh
+++ jenkins/debian_c.sh
@@ -18,11 +18,18 @@ build_folder=$build_root"/cmake/debian_linux"
 # Set the default cores
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
 
+if [ $CORES -eq 1 ]
+then
+    LOADLIMIT=$CORES
+else
+    LOADLIMIT=$(perl -e "print int($CORES * 0.8)")
+fi
+
 rm -r -f $build_folder
 mkdir -p $build_folder
 pushd $build_folder
 cmake -Drun_valgrind:BOOL=ON $build_root -Drun_unittests:BOOL=ON
-make --jobs=$CORES
+make --jobs=$CORES --max-load=$LOADLIMIT
 
 #use doctored openssl
 export LD_LIBRARY_PATH=/usr/local/ssl/lib
