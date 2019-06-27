592e88e268963387d79e1974aa337a3770132649
jbobotek <jeremy.bobotek@gmail.com>
2019-06-27 10:41:28 -0700

    Update build.sh

--- devops/scripts/linux/build.sh
+++ devops/scripts/linux/build.sh
@@ -5,7 +5,7 @@
 set -e
 
 script_dir=$(cd "$(dirname "$0")" && pwd)
-build_root=$(cd "${script_dir}/../.." && pwd)
+build_root=$(cd "${script_dir}/../" && pwd)
 log_dir=$build_root
 run_e2e_tests=OFF
 run_sfc_tests=OFF
