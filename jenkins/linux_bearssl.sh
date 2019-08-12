429eaacefa2b66bbb23ce45b472c00df1f68af4a
jbobotek <jeremy.bobotek@gmail.com>
2019-08-12 12:31:27 -0700

    Update linux_bearssl.sh

--- jenkins/linux_bearssl.sh
+++ jenkins/linux_bearssl.sh
@@ -11,7 +11,7 @@ curl --version
 
 # Set the default cores
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
-cmake . -Bcmake -Duse_bearssl:BOOL=ON -Drun_e2e_tests:BOOL=ON
+cmake . -Bcmake -Duse_bearssl:BOOL=ON -Drun_e2e_tests:BOOL=ON -Duse_http:BOOL:OFF
 cd cmake
 
 make --jobs=$CORES
