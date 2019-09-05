14d6934e2bdee714dd3bbfbce85595997687f653
Dane Walton <dawalton@microsoft.com>
2019-09-05 08:39:50 -0700

    change placement

--- iothub_client/src/iothubtransport_mqtt_common.c
+++ iothub_client/src/iothubtransport_mqtt_common.c
@@ -2315,7 +2315,7 @@ static int InitializeConnection(PMQTTTRANSPORT_HANDLE_DATA transport_data)
         if (transport_data->mqttClientStatus == MQTT_CLIENT_STATUS_NOT_CONNECTED && transport_data->isRecoverableError)
         {
             // Note: in case retry_control_should_retry fails, the reconnection shall be attempted anyway (defaulting to policy IOTHUB_CLIENT_RETRY_IMMEDIATE).
-            if (retry_control_should_retry(transport_data->retry_control_handle, &retry_action) != 0 || retry_action == RETRY_ACTION_RETRY_NOW || transport_data->connectFailCount == 0)
+            if(transport_data->connectFailCount == 0 || retry_control_should_retry(transport_data->retry_control_handle, &retry_action) != 0 || retry_action == RETRY_ACTION_RETRY_NOW)
             {
                 if (tickcounter_get_current_ms(transport_data->msgTickCounter, &transport_data->connectTick) != 0)
                 {
