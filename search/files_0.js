7774cd5df8cbc758ecf1ba09b1b18a1538bd7cf4
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2019-10-09 09:25:11 -0700

    master

--- search/files_0.js
+++ search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['agenttime_2eh',['agenttime.h',['../agenttime_8h.html',1,'']]]
+  ['agenttime_2eh',['agenttime.h',['../agenttime_8h.html',1,'']]],
+  ['azure_5fbase64_2eh',['azure_base64.h',['../azure__base64_8h.html',1,'']]]
 ];
