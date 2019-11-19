da0e69ee79f64dcb7af3e34fe2976681fe1f06dd
Jasmine Lo <jasminel@microsoft.com>
2019-11-19 15:14:30 -0800

    define defaults value for device info sample. #1325

--- digitaltwin_client/samples/digitaltwin_sample_device_info/ops/deviceinfo_ops_sample.c
+++ digitaltwin_client/samples/digitaltwin_sample_device_info/ops/deviceinfo_ops_sample.c
@@ -1,75 +1,77 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "azure_c_shared_utility/xlogging.h"
 #include "../digitaltwin_sample_device_info.h"
 #include "deviceinfo_ops.h"
 
-// Sample values
-static const char digitaltwinSample_SoftwareVersionData[] = "software-revision-1.0.2A";
-static const char digitaltwinSample_ManufacturerData[] = "Sample Manufacturer for Azure IoT C SDK enabled device";
-static const char digitaltwinSample_ModelData[] = "Sample Model 123";
-static const char digitaltwinSample_OsNameData[] = "sample-OperatingSystem-name";
-static const char digitaltwinSample_ProcessorArchitectureData[] = "Contoso-Arch-64bit";
-static const char digitaltwinSample_ProcessorManufacturerData[] = "Processor Manufacturer(TM)";
-static const int  digitaltwinSample_TotalStorageData = 83886080; // 80GB
-static const int  digitaltwinSample_TotalMemoryData = 32768; // 32 MB
+#ifndef DIGITALTWIN_SAMPLE_SOFTWARE_VERSION
+#define DIGITALTWIN_SAMPLE_SOFTWARE_VERSION "1.0.0.0"
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_MANUFACTURER
+#define DIGITALTWIN_SAMPLE_MANUFACTURER "Sample-Manufacturer"
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_MODEL
+#define DIGITALTWIN_SAMPLE_MODEL "Sample-Model-123"
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_OS_NAME
+#define DIGITALTWIN_SAMPLE_OS_NAME "sample-OperatingSystem-name"
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_PROCESSOR_ARCHITECTURE
+#define DIGITALTWIN_SAMPLE_PROCESSOR_ARCHITECTURE "Contoso-Arch-64bit"
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_PROCESSOR_MANUFACTURER
+#define DIGITALTWIN_SAMPLE_PROCESSOR_MANUFACTURER "Processor Manufacturer(TM)"
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_TOTAL_STORAGE
+#define DIGITALTWIN_SAMPLE_TOTAL_STORAGE 83886080 // 80GB
+#endif
+
+#ifndef DIGITALTWIN_SAMPLE_TOTAL_MEMORY
+#define DIGITALTWIN_SAMPLE_TOTAL_MEMORY 32768 // 32 MB
+#endif
 
 int DeviceInformation_SwVersionProperty(char ** returnValue)
-{
-    const char* swVersion = digitaltwinSample_SoftwareVersionData;
-    
-    return DI_serializeCharData(swVersion, returnValue);
+{   
+    return DI_serializeCharData(DIGITALTWIN_SAMPLE_SOFTWARE_VERSION, returnValue);
 }
 
 int DeviceInformation_ManufacturerProperty(char ** returnValue)
-{
-    const char* manufacturer = digitaltwinSample_ManufacturerData;
-    
-    return DI_serializeCharData(manufacturer, returnValue);
+{   
+    return DI_serializeCharData(DIGITALTWIN_SAMPLE_MANUFACTURER, returnValue);
 }
 
 int DeviceInformation_ModelProperty(char ** returnValue)
-{
-    const char* model = digitaltwinSample_ModelData;
-    
-    return DI_serializeCharData(model, returnValue);
+{   
+    return DI_serializeCharData(DIGITALTWIN_SAMPLE_MODEL, returnValue);
 }
 
 int DeviceInformation_OsNameProperty(char ** returnValue)
-{
-    const char* osName = digitaltwinSample_OsNameData;
-    
-    return DI_serializeCharData(osName, returnValue);
+{   
+    return DI_serializeCharData(DIGITALTWIN_SAMPLE_OS_NAME, returnValue);
 }
 
 int DeviceInformation_ProcessorArchitectureProperty(char ** returnValue)
 {
-    const char* processArch = digitaltwinSample_ProcessorArchitectureData;
-    
-    return DI_serializeCharData(processArch, returnValue);
+    return DI_serializeCharData(DIGITALTWIN_SAMPLE_PROCESSOR_ARCHITECTURE, returnValue);
 }
 
 int DeviceInformation_ProcessorManufacturerProperty(char ** returnValue)
-{
-    const char* processorManufacturer = digitaltwinSample_ProcessorManufacturerData;
-    
-    return DI_serializeCharData(processorManufacturer, returnValue);
+{   
+    return DI_serializeCharData(DIGITALTWIN_SAMPLE_PROCESSOR_MANUFACTURER, returnValue);
 }
 
 int DeviceInformation_TotalStorageProperty(char ** returnValue)
-{
-    const int totalStorage = digitaltwinSample_TotalStorageData;
-    
-    return DI_serializeIntegerData(totalStorage, returnValue);
+{ 
+    return DI_serializeIntegerData(DIGITALTWIN_SAMPLE_TOTAL_STORAGE, returnValue);
 }
 
 int DeviceInformation_TotalMemoryProperty(char ** returnValue)
 {
-    const int totalMemory = digitaltwinSample_TotalMemoryData;
-    
-    return DI_serializeIntegerData(totalMemory, returnValue);
+    return DI_serializeIntegerData(DIGITALTWIN_SAMPLE_TOTAL_MEMORY, returnValue);
 }
