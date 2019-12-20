fc9177ec3d343cc9e01b7519cea98fd8c025f0b9
jbobotek <jeremy.bobotek@gmail.com>
2019-12-19 17:01:23 -0800

    Update setup_arduino_libraries.sh

--- build_all/arduino/setup_arduino_libraries.sh
+++ build_all/arduino/setup_arduino_libraries.sh
@@ -1,3 +1,4 @@
+echo $NEWTIN | sudo -S rm -r azure-iot-pal-arduino
 git clone -b make-sdk-python https://github.com/Azure/azure-iot-pal-arduino.git 
 git submodule update --init --recursive
 
@@ -10,6 +11,6 @@ python3 make_sdk.py -o $ARDUINO_LIBRARY_DIR -d esp32
 # input WIFI_SSID, WIFI_PWD, and IOTHUB_CONNECTION_DEVICE_STRING into iot_config.h
 
 cd $ARDUINO_LIBRARY_DIR/AzureIoTHub/examples/
-sed -i 's/your-wifi-name/'$WIFI_SSID'/g' *
-sed -i 's/your-wifi-pwd/'$WIFI_PWD'/g' *
-sed -i 's/your-iothub-DEVICE-connection-string/'$IOTHUB_CONNECTION_DEVICE_STRING'/g' *
+echo $NEWTIN | sudo -S sed -i 's/your-wifi-name/'$WIFI_SSID'/g' *
+echo $NEWTIN | sudo -S sed -i 's/your-wifi-pwd/'$WIFI_PWD'/g' *
+echo $NEWTIN | sudo -S sed -i 's/your-iothub-DEVICE-connection-string/'$IOTHUB_CONNECTION_DEVICE_STRING'/g' *
