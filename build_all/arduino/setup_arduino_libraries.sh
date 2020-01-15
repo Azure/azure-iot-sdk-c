bb9db93f65b538f8b03b7c14768df4e915f2587d
jbobotek <jeremy.bobotek@gmail.com>
2020-01-14 20:58:46 -0800

    Update setup_arduino_libraries.sh

--- build_all/arduino/setup_arduino_libraries.sh
+++ build_all/arduino/setup_arduino_libraries.sh
@@ -17,6 +17,6 @@ python3 make_sdk.py -o $ARDUINO_LIBRARY_DIR
 # input WIFI_SSID, WIFI_PWD, and IOTHUB_CONNECTION_DEVICE_STRING into iot_config.h
 
 cd $ARDUINO_LIBRARY_DIR/AzureIoTHub/examples/iothub_ll_telemetry_sample/
-echo $NEWTIN | sudo -S sed -i 's/your-wifi-name/'$WIFI_SSID'/g' *
-echo $NEWTIN | sudo -S sed -i 's/your-wifi-pwd/'$WIFI_PWD'/g' *
-echo $NEWTIN | sudo -S sed -i 's@your-iothub-DEVICE-connection-string@'$IOTHUB_CONNECTION_DEVICE_STRING'@g' *
+echo $NEWTIN | sudo -S sed -i 's/your-wifi-name/'$WIFI_SSID'/g' iot_configs.h
+echo $NEWTIN | sudo -S sed -i 's/your-wifi-pwd/'$WIFI_PWD'/g' iot_configs.h
+echo $NEWTIN | sudo -S sed -i 's@your-iothub-DEVICE-connection-string@'$IOTHUB_CONNECTION_DEVICE_STRING'@g' iot_configs.h
