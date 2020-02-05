f1e6ebb40ab5692505ece145388f8cb7db8c42bf
Jasmine Lo <jasminel@microsoft.com>
2020-02-05 13:58:26 -0800

    Install nodejs on builds.

--- jenkins/linux_bearssl.sh
+++ jenkins/linux_bearssl.sh
@@ -4,10 +4,16 @@
 
 set -e
 
+#install nodejs
+sudo curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
 # Print version
 cat /etc/*release | grep VERSION*
 gcc --version
 curl --version
+node --version
+npm --version
 
 # Set the default cores
 CORES=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || sysctl -n hw.ncpu)
--- jenkins/ubuntu1604_c.sh
+++ jenkins/ubuntu1604_c.sh
@@ -2,11 +2,17 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#install nodejs
+sudo curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
 # Print version
 cat /etc/*release | grep VERSION*
 gcc --version
 openssl version
 curl --version
+node --version
+npm --version
 
 build_root=$(cd "$(dirname "$0")/.." && pwd)
 cd $build_root
