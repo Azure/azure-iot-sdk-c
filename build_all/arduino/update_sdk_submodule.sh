e96fc867dd72c0531643a7d7c7d3f3259205d46a
jbobotek <jeremy.bobotek@gmail.com>
2020-02-11 13:47:06 -0800

    Update update_sdk_submodule.sh

--- build_all/arduino/update_sdk_submodule.sh
+++ build_all/arduino/update_sdk_submodule.sh
@@ -3,6 +3,7 @@
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 # check out pipeline branch
 
+cd sdk
 echo "in sdk now"
 ls
 git pull origin master
