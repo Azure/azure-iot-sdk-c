dd2b51494738088a3667667883564016a177a559
jbobotek <jeremy.bobotek@gmail.com>
2020-02-10 18:18:57 -0800

    Update setup_arduino_libraries.sh

--- build_all/arduino/setup_arduino_libraries.sh
+++ build_all/arduino/setup_arduino_libraries.sh
@@ -5,7 +5,7 @@ echo $NEWTIN | sudo -S rm -r azure-iot-pal-arduino
 git clone https://github.com/Azure/azure-iot-pal-arduino.git 
 
 cd azure-iot-pal-arduino
-git submodule update --init
+git submodule update --init --recursive
 rsync -avz --existing ./ sdk/
 
 # check out pipeline branch
