d83e8fff21b3c96fadcea8f9a97a0f848c5e56f4
jbobotek <jeremy.bobotek@gmail.com>
2020-02-11 13:44:12 -0800

    Create update_sdk_submodule.sh

---
+++ build_all/arduino/update_sdk_submodule.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+# check out pipeline branch
+
+echo "in sdk now"
+ls
+git pull origin master
+git checkout $SDK_BRANCH_NAME
+git submodule update --init --recursive
+cd ../build_all
+git submodule status
+
+python3 make_sdk.py -o $ARDUINO_LIBRARY_DIR
+
+# input WIFI_SSID, WIFI_PWD, and IOTHUB_CONNECTION_DEVICE_STRING into iot_config.h
+
+cd $ARDUINO_LIBRARY_DIR/AzureIoTHub/examples/iothub_ll_telemetry_sample/
+echo $NEWTIN | sudo -S sed -i 's/your-wifi-name/'$WIFI_SSID'/g' iot_configs.h
+echo $NEWTIN | sudo -S sed -i 's/your-wifi-pwd/'$WIFI_PWD'/g' iot_configs.h
+echo $NEWTIN | sudo -S sed -i 's@your-iothub-DEVICE-connection-string@'$IOTHUB_CONNECTION_DEVICE_STRING'@g' iot_configs.h
