25a48ca74065b35df9c5699da3a8913188e688ad
jbobotek <jeremy.bobotek@gmail.com>
2020-02-13 13:54:49 -0800

    Update setup_arduino_libraries.sh

--- build_all/arduino/setup_arduino_libraries.sh
+++ build_all/arduino/setup_arduino_libraries.sh
@@ -8,7 +8,7 @@ git clone https://github.com/Azure/azure-iot-pal-arduino.git
 cd azure-iot-pal-arduino
 git submodule update --init --recursive
 echo "I am copying sources now"
-echo $NEWTIN | sudo -S cp -r $SOURCES_DIR sdk
+echo $NEWTIN | sudo -S cp -r $SOURCES_DIR sdk/
 # rsync -avz --existing ./ sdk/
 
 cd build_all
