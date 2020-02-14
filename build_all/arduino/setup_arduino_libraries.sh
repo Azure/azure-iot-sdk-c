9445b2a14c16d721dde9fd301c132f91e685da4b
jbobotek <jeremy.bobotek@gmail.com>
2020-02-14 14:05:54 -0800

    Jbobotek patch mxchip iotz (#1431)
    * Update setup.sh
    
    * Update build.sh
    
    * Update .mxchip-ci.yml for Azure Pipelines
    
    * Update arduino-esp.yml for Azure Pipelines
    
    * Update setup_arduino_libraries.sh
    
    * Delete unnecessary .sh, update_sdk_submodule.sh
    
    * Update setup_arduino_libraries.sh
    
    * Update setup_arduino_libraries.sh

--- build_all/arduino/setup_arduino_libraries.sh
+++ build_all/arduino/setup_arduino_libraries.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
+cd $STAGING_DIR
 echo $NEWTIN | sudo -S rm -r azure-iot-pal-arduino
 git clone https://github.com/Azure/azure-iot-pal-arduino.git 
 
 cd azure-iot-pal-arduino
-
-git submodule update --init --recursive
-
-rsync -avz --existing ./ sdk/
+echo "copying sdk sources path to pal layer now"
+echo $NEWTIN | sudo -S cp -r $SOURCES_DIR/. sdk/
 
 cd build_all
 
