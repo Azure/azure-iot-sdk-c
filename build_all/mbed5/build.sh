68bc4f58c24b4a68c2e15969610c337b9683cc27
jbobotek <jeremy.bobotek@gmail.com>
2020-04-07 19:47:35 -0700

    Update build.sh

--- build_all/mbed5/build.sh
+++ build_all/mbed5/build.sh
@@ -5,12 +5,12 @@ sudo rm -R -f mxchip
 git clone -b $MXCHIP_REPO_SOURCE_BRANCH https://github.com/massand/devkit-mbedos5-getstarted.git mxchip
 cd mxchip
 // update mbed url with credentials
-echo $NEWTIN | sudo -S sed -i 's&azure-iot-sdks&'v-jebobo:$MBED_CRED'&g' .gitmodules
+echo $NEWTIN | sudo -S sed -i 's&azure-iot-sdks&'$MBED_USER:$MBED_CRED'&g' .gitmodules
 git submodule update --init
 cd mbed-iot-devkit-sdk
 // update devkit-azure-iot-sdk-c and mbed-az3166-driver with credentials
-echo $NEWTIN | sudo -S sed -i 's&mseng@&'v-jebobo:$DEVKIT_CRED@'&g' .gitmodules
-echo $NEWTIN | sudo -S sed -i 's&placehold&'v-jebobo:$AZ3166_CRED'&g' .gitmodules
+echo $NEWTIN | sudo -S sed -i 's&mseng@&'$DEVKIT_USER:$DEVKIT_CRED@'&g' .gitmodules
+echo $NEWTIN | sudo -S sed -i 's&placehold&'$AZ3166_USER:$AZ3166_CRED'&g' .gitmodules
 git submodule update --init --recursive
 cd ..
 rsync -avz --existing ./ mbed-iot-devkit-sdk/components/azure-iot-sdk-c/
