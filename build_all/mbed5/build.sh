aee11b8e052e5b83341d3ff3c82da11b6afb04d7
jbobotek <jeremy.bobotek@gmail.com>
2020-08-01 07:19:17 -0700

    Update build.sh

--- build_all/mbed5/build.sh
+++ build_all/mbed5/build.sh
@@ -24,7 +24,6 @@ rsync -avz --existing ./ mbed-iot-devkit-sdk/components/azure-iot-sdk-c/
 
 sudo npm install -g iotz
 sudo iotz update
-npm list -g
 sudo cp $SOURCES_DIRECTORY/iotz/extensions/mbed/index.js /usr/local/lib/node_modules/iotz/extensions/mbed/index.js
 
 echo "just before init iotz mbed"
