19475a6fd69a9e210b8e6342f56498097b867ea6
John Spaith <jspaith@windows.microsoft.com>
2020-10-07 09:03:49 -0600

    Document archival of public-preview-pnp

--- digitaltwin_client/tests/dt_e2e/service/mocha_exec.cmd
+++
@@ -1,7 +0,0 @@
-call npm install
-call dir
-
-call node_modules\.bin\tsc.cmd dt_e2e_test.ts iothub_test_core.ts
-IF EXIST ..\device\Release ( node_modules\.bin\mocha.cmd dt_e2e_test.js --connectionString %IOTHUB_CONNECTION_STRING% --e "..\\device\\Release\\dt_e2e_exe.exe" --timeout 120000 
-) ELSE ( node_modules\.bin\mocha.cmd dt_e2e_test.js --connectionString %IOTHUB_CONNECTION_STRING% --e "..\\device\\Debug\\dt_e2e_exe.exe" --timeout 120000 )
-)
