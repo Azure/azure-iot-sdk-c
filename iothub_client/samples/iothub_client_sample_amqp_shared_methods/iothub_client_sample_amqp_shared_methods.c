a63f60a0a218b75291ee7a32e5e77cf2d871d31e
John Spaith <jspaith@windows.microsoft.com>
2020-10-22 21:32:14 -0600

    Doc away from the legacy 'client' to the 'device_client' replacement.

--- iothub_client/samples/iothub_client_sample_amqp_shared_methods/iothub_client_sample_amqp_shared_methods.c
+++ iothub_client/samples/iothub_client_sample_amqp_shared_methods/iothub_client_sample_amqp_shared_methods.c
@@ -131,15 +131,15 @@ int main(void)
             /* Here subscribe for C2D methods */
             if (IoTHubDeviceClient_SetDeviceMethodCallback(device_handle1, DeviceMethodCallback, (void*)deviceId1) != IOTHUB_CLIENT_OK)
             {
-                (void)printf("ERROR: IoTHubClient_SetDeviceMethodCallback for the first device..........FAILED!\r\n");
+                (void)printf("ERROR: IoTHubDeviceClient_SetDeviceMethodCallback for the first device..........FAILED!\r\n");
             }
             else if (IoTHubDeviceClient_SetDeviceMethodCallback(device_handle2, DeviceMethodCallback, (void*)deviceId2) != IOTHUB_CLIENT_OK)
             {
-                (void)printf("ERROR: IoTHubClient_SetDeviceMethodCallback for the second device..........FAILED!\r\n");
+                (void)printf("ERROR: IoTHubDeviceClient_SetDeviceMethodCallback for the second device..........FAILED!\r\n");
             }
             else
             {
-                (void)printf("IoTHubClient_SetMessageCallback...successful.\r\n");
+                (void)printf("IoTHubDeviceClient_SetMessageCallback...successful.\r\n");
                 (void)printf("Waiting for C2D methods ...\r\n");
 
                 (void)printf("Press enter to close application\r\n");
