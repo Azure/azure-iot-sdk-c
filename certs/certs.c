612714126b00a622f0ec63d89f53c01a6ebbb189
Mollie Munoz <momuno@microsoft.com>
2021-01-21 12:36:12 -0800

    Update comment

--- certs/certs.c
+++ certs/certs.c
@@ -6,7 +6,7 @@
 #include "certs.h"
 /* Note: for devices with limited resources, only one cert should be loaded.
    #defines are used to reduce memory footprint of certificates.
-   To use ECC with IoT Hub Gateway V2, use -DUSE_DIGICERT_G3_CERT.
+   To use ECC with IoT Hub Gateway V2, please build with -DUSE_DIGICERT_G3_CERT.
    For DE and CN regions, please build with -DUSE_MICROSOFTAZURE_DE_CERT or -DUSE_PORTAL_AZURE_CN_CERT, respectively,
    if you wish to load ONLY those certs.
 */
