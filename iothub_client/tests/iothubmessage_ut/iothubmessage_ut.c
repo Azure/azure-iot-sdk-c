fedcf1e460fc1a348f9d0a3a84f6e764562ffc37
John Spaith <jspaith@windows.microsoft.com>
2021-09-10 09:37:06 -0700

    Fix merge issue

--- iothub_client/tests/iothubmessage_ut/iothubmessage_ut.c
+++ iothub_client/tests/iothubmessage_ut/iothubmessage_ut.c
@@ -2305,6 +2305,7 @@ TEST_FUNCTION(IoTHubMessage_Destroy_with_context_Succeed)
     STRICT_EXPECTED_CALL(gballoc_free(IGNORED_PTR_ARG));
     STRICT_EXPECTED_CALL(gballoc_free(IGNORED_PTR_ARG));
     STRICT_EXPECTED_CALL(gballoc_free(IGNORED_PTR_ARG));
+    STRICT_EXPECTED_CALL(gballoc_free(IGNORED_PTR_ARG));
     STRICT_EXPECTED_CALL(gballoc_free(h));
 
     //act
