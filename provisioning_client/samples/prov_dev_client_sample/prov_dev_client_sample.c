868db9a4650d7fb22b89cc170a7e6917e84f7c13
Cristian Pop <CIPop@users.noreply.github.com>
2021-11-05 15:45:52 -0700

    Update provisioning_client/samples/prov_dev_client_sample/prov_dev_client_sample.c
    Co-authored-by: Dane Walton <dawalton@microsoft.com>

--- provisioning_client/samples/prov_dev_client_sample/prov_dev_client_sample.c
+++ provisioning_client/samples/prov_dev_client_sample/prov_dev_client_sample.c
@@ -104,7 +104,7 @@ int main()
 
     // Set the symmetric key if using they auth type
     // If using DPS with an enrollment group, this must the the derived device key from the DPS Primary Key
-    // https://docs.microsoft.com/en-us/azure/iot-dps/concepts-symmetric-key-attestation?tabs=azure-cli#group-enrollments
+    // https://docs.microsoft.com/azure/iot-dps/concepts-symmetric-key-attestation?tabs=azure-cli#group-enrollments
     //prov_dev_set_symmetric_key_info("<symm_registration_id>", "<symmetric_Key>");
 
     HTTP_PROXY_OPTIONS http_proxy;
