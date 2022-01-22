deaf8af4dd1871d8791cefc09f79e43a30d8cbb7
momuno <momuno@users.noreply.github.com>
2022-01-22 00:11:45 +0000

    Deploying to gh-pages from @ Azure/azure-iot-sdk-c@7329f70906180fd7da1f8b47fb9f8cd39a8ef1ff 🚀

--- search/all_6.js
+++ search/all_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['multiplexing_20limitations_20and_20alternatives_217',['Multiplexing limitations and alternatives',['../md__home_runner_work_azure_iot_sdk_c_azure_iot_sdk_c_doc_multiplexing_limitations.html',1,'']]]
+  ['master_20to_20main_20branch_20rename_20instructions_217',['master to main branch rename instructions',['../md__home_runner_work_azure_iot_sdk_c_azure_iot_sdk_c_doc_master_to_main_rename.html',1,'']]],
+  ['multiplexing_20limitations_20and_20alternatives_218',['Multiplexing limitations and alternatives',['../md__home_runner_work_azure_iot_sdk_c_azure_iot_sdk_c_doc_multiplexing_limitations.html',1,'']]]
 ];
--- search/all_7.js
+++ search/all_7.js
@@ -1,18 +1,18 @@
 var searchData=
 [
-  ['prepare_20your_20development_20environment_218',['Prepare your development environment',['../md__home_runner_work_azure_iot_sdk_c_azure_iot_sdk_c_doc_devbox_setup.html',1,'']]],
-  ['protocol_219',['protocol',['../struct_i_o_t_h_u_b___c_l_i_e_n_t___c_o_n_f_i_g.html#aede343b62f08a21321e3ca1db1280554',1,'IOTHUB_CLIENT_CONFIG::protocol()'],['../struct_i_o_t_h_u_b___c_l_i_e_n_t___d_e_v_i_c_e___c_o_n_f_i_g.html#ab45243fbdffb81250c1cd99cad4ebd93',1,'IOTHUB_CLIENT_DEVICE_CONFIG::protocol()']]],
-  ['protocolgatewayhostname_220',['protocolGatewayHostName',['../struct_i_o_t_h_u_b___c_l_i_e_n_t___c_o_n_f_i_g.html#a763edfe7082b865140332d5e8f7f1b85',1,'IOTHUB_CLIENT_CONFIG']]],
-  ['prov_5fdevice_5fclient_2eh_221',['prov_device_client.h',['../prov__device__client_8h.html',1,'']]],
-  ['prov_5fdevice_5fget_5fprovisioning_5fpayload_222',['Prov_Device_Get_Provisioning_Payload',['../prov__device__client_8h.html#a74b6604572755ada405fff1fcf6a35af',1,'prov_device_client.h']]],
-  ['prov_5fdevice_5fll_5fclient_2eh_223',['prov_device_ll_client.h',['../prov__device__ll__client_8h.html',1,'']]],
-  ['prov_5fdevice_5fll_5fcreate_224',['Prov_Device_LL_Create',['../prov__device__ll__client_8h.html#a9f209da792720540561a27a5f82d62b2',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fdestroy_225',['Prov_Device_LL_Destroy',['../prov__device__ll__client_8h.html#a3634dea8a59ee510065e457a93e5c073',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fdowork_226',['Prov_Device_LL_DoWork',['../prov__device__ll__client_8h.html#a32587f168929aee7108416ebbee082d3',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fget_5fprovisioning_5fpayload_227',['Prov_Device_LL_Get_Provisioning_Payload',['../prov__device__ll__client_8h.html#a933b41c5865ea5af0249929f0632344d',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fgetversionstring_228',['Prov_Device_LL_GetVersionString',['../prov__device__ll__client_8h.html#aec9675ed8dcfcc52b04e9a6a0f2e6a01',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fregister_5fdevice_229',['Prov_Device_LL_Register_Device',['../prov__device__ll__client_8h.html#ad466d9cb0750eefc15bafe006018ae6c',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fset_5fprovisioning_5fpayload_230',['Prov_Device_LL_Set_Provisioning_Payload',['../prov__device__ll__client_8h.html#a0343043c1875c3555720796942126ba5',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fll_5fsetoption_231',['Prov_Device_LL_SetOption',['../prov__device__ll__client_8h.html#ae5035b58c14ec28a988f90eee182f0fd',1,'prov_device_ll_client.h']]],
-  ['prov_5fdevice_5fset_5fprovisioning_5fpayload_232',['Prov_Device_Set_Provisioning_Payload',['../prov__device__client_8h.html#a0ecb4ce2d9a4aba1467266d9c6a58402',1,'prov_device_client.h']]]
+  ['prepare_20your_20development_20environment_219',['Prepare your development environment',['../md__home_runner_work_azure_iot_sdk_c_azure_iot_sdk_c_doc_devbox_setup.html',1,'']]],
+  ['protocol_220',['protocol',['../struct_i_o_t_h_u_b___c_l_i_e_n_t___c_o_n_f_i_g.html#aede343b62f08a21321e3ca1db1280554',1,'IOTHUB_CLIENT_CONFIG::protocol()'],['../struct_i_o_t_h_u_b___c_l_i_e_n_t___d_e_v_i_c_e___c_o_n_f_i_g.html#ab45243fbdffb81250c1cd99cad4ebd93',1,'IOTHUB_CLIENT_DEVICE_CONFIG::protocol()']]],
+  ['protocolgatewayhostname_221',['protocolGatewayHostName',['../struct_i_o_t_h_u_b___c_l_i_e_n_t___c_o_n_f_i_g.html#a763edfe7082b865140332d5e8f7f1b85',1,'IOTHUB_CLIENT_CONFIG']]],
+  ['prov_5fdevice_5fclient_2eh_222',['prov_device_client.h',['../prov__device__client_8h.html',1,'']]],
+  ['prov_5fdevice_5fget_5fprovisioning_5fpayload_223',['Prov_Device_Get_Provisioning_Payload',['../prov__device__client_8h.html#a74b6604572755ada405fff1fcf6a35af',1,'prov_device_client.h']]],
+  ['prov_5fdevice_5fll_5fclient_2eh_224',['prov_device_ll_client.h',['../prov__device__ll__client_8h.html',1,'']]],
+  ['prov_5fdevice_5fll_5fcreate_225',['Prov_Device_LL_Create',['../prov__device__ll__client_8h.html#a9f209da792720540561a27a5f82d62b2',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fdestroy_226',['Prov_Device_LL_Destroy',['../prov__device__ll__client_8h.html#a3634dea8a59ee510065e457a93e5c073',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fdowork_227',['Prov_Device_LL_DoWork',['../prov__device__ll__client_8h.html#a32587f168929aee7108416ebbee082d3',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fget_5fprovisioning_5fpayload_228',['Prov_Device_LL_Get_Provisioning_Payload',['../prov__device__ll__client_8h.html#a933b41c5865ea5af0249929f0632344d',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fgetversionstring_229',['Prov_Device_LL_GetVersionString',['../prov__device__ll__client_8h.html#aec9675ed8dcfcc52b04e9a6a0f2e6a01',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fregister_5fdevice_230',['Prov_Device_LL_Register_Device',['../prov__device__ll__client_8h.html#ad466d9cb0750eefc15bafe006018ae6c',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fset_5fprovisioning_5fpayload_231',['Prov_Device_LL_Set_Provisioning_Payload',['../prov__device__ll__client_8h.html#a0343043c1875c3555720796942126ba5',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fll_5fsetoption_232',['Prov_Device_LL_SetOption',['../prov__device__ll__client_8h.html#ae5035b58c14ec28a988f90eee182f0fd',1,'prov_device_ll_client.h']]],
+  ['prov_5fdevice_5fset_5fprovisioning_5fpayload_233',['Prov_Device_Set_Provisioning_Payload',['../prov__device__client_8h.html#a0ecb4ce2d9a4aba1467266d9c6a58402',1,'prov_device_client.h']]]
 ];
