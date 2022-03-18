d251c2153887b64a6519922780f87376ccd00345
Eric Wolz <ericwol@microsoft.com>
2022-03-18 11:41:10 -0700

    change ctest to use memcheck

--- build_all/linux/run_tests.sh
+++ build_all/linux/run_tests.sh
@@ -12,5 +12,5 @@ TEST_CORES=16
 # Refresh dynamic libs to link to
 sudo ldconfig
 
-ctest -T test --no-compress-output -C "Debug" -V -j $TEST_CORES --schedule-random
+ctest -T MemCheck --no-compress-output -C "Debug" -V -j $TEST_CORES --schedule-random
 
