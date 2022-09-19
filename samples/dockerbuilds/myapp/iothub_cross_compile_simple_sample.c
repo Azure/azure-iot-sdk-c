bba747bce59ac77a46cc9a71404f65ee62075ff4
Eric Wolz <ericwol@microsoft.com>
2022-09-18 20:12:38 -0700

    Update ARM/MIPS dockerfile and adjunct

--- samples/dockerbuilds/myapp/iothub_cross_compile_simple_sample.c
+++ samples/dockerbuilds/myapp/iothub_cross_compile_simple_sample.c
@@ -12,15 +12,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "iothub.h"
-#include "iothub_device_client.h"
-#include "iothub_client_options.h"
-#include "iothub_message.h"
+#include "azureiot/iothub.h"
+#include "azureiot/iothub_device_client.h"
+#include "azureiot/iothub_client_options.h"
+#include "azureiot/iothub_message.h"
 #include "azure_c_shared_utility/threadapi.h"
 #include "azure_c_shared_utility/crt_abstractions.h"
 #include "azure_c_shared_utility/platform.h"
 #include "azure_c_shared_utility/shared_util_options.h"
-#include "iothubtransportmqtt.h"
+#include "azureiot/iothubtransportmqtt.h"
 
 /* This sample uses the convenience APIs of iothub_client for example purposes. */
 
