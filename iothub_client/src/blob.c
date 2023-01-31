702b4984ac471fd1c98f1204a4420c290abddb28
Valerie Avva Lim <vaavva@microsoft.com>
2023-01-30 18:40:32 -0800

    retain blob upload timeout if set

--- iothub_client/src/blob.c
+++ iothub_client/src/blob.c
@@ -279,7 +279,7 @@ static BLOB_RESULT SendBlockIdList(HTTPAPIEX_HANDLE httpApiExHandle, const char*
 }
 
 
-BLOB_RESULT Blob_UploadMultipleBlocksFromSasUri(const char* SASURI, IOTHUB_CLIENT_FILE_UPLOAD_GET_DATA_CALLBACK_EX getDataCallbackEx, void* context, unsigned int* httpStatus, BUFFER_HANDLE httpResponse, const char* certificates, HTTP_PROXY_OPTIONS *proxyOptions, const char* networkInterface)
+BLOB_RESULT Blob_UploadMultipleBlocksFromSasUri(const char* SASURI, IOTHUB_CLIENT_FILE_UPLOAD_GET_DATA_CALLBACK_EX getDataCallbackEx, void* context, unsigned int* httpStatus, BUFFER_HANDLE httpResponse, const char* certificates, HTTP_PROXY_OPTIONS *proxyOptions, const char* networkInterface, const size_t* timeout)
 {
     BLOB_RESULT result;
     const char* hostnameBegin;
@@ -327,6 +327,11 @@ BLOB_RESULT Blob_UploadMultipleBlocksFromSasUri(const char* SASURI, IOTHUB_CLIEN
                 {
                     LogError("unable to create a HTTPAPIEX_HANDLE");
                     result = BLOB_ERROR;
+                }
+                else if ((timeout != NULL) && (HTTPAPIEX_SetOption(httpApiExHandle, "timeout", timeout) == HTTPAPIEX_ERROR))
+                {
+                    LogError("unable to set blob transfer timeout");
+                    result = IOTHUB_CLIENT_ERROR;
                 }
                 else if ((certificates != NULL) && (HTTPAPIEX_SetOption(httpApiExHandle, "TrustedCerts", certificates) == HTTPAPIEX_ERROR))
                 {
--- iothub_client/src/iothub_client_ll_uploadtoblob.c
+++ iothub_client/src/iothub_client_ll_uploadtoblob.c
@@ -745,7 +745,8 @@ IOTHUB_CLIENT_RESULT IoTHubClient_LL_UploadMultipleBlocksToBlob_Impl(IOTHUB_CLIE
                                         }
                                         else
                                         {
-                                            BLOB_RESULT uploadMultipleBlocksResult = Blob_UploadMultipleBlocksFromSasUri(STRING_c_str(sasUri), getDataCallbackEx, context, &httpResponse, responseToIoTHub, upload_data->certificates, &(upload_data->http_proxy_options), upload_data->networkInterface);
+                                            size_t timeout = ((long)upload_data->blob_upload_timeout_secs * 1000);
+                                            BLOB_RESULT uploadMultipleBlocksResult = Blob_UploadMultipleBlocksFromSasUri(STRING_c_str(sasUri), getDataCallbackEx, context, &httpResponse, responseToIoTHub, upload_data->certificates, &(upload_data->http_proxy_options), upload_data->networkInterface, &timeout);
                                             if (uploadMultipleBlocksResult == BLOB_ABORTED)
                                             {
                                                 LogInfo("Blob_UploadFromSasUri aborted file upload");
