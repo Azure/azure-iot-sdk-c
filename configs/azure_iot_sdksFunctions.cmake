a6d4cb71f04a9e142b8e628e73ec623c90d8d492
Eric Wolz <ericwol@microsoft.com>
2023-03-13 15:46:26 -0700

    Update to linux .so

--- configs/azure_iot_sdksFunctions.cmake
+++ configs/azure_iot_sdksFunctions.cmake
@@ -230,3 +230,35 @@ function(printCmakeArgs)
     endforeach()
     message("CMAKE_ARGS: ${CMAKE_ARGS}")
 endfunction()
+
+function(print_properties)
+    message("CMAKE_PROPERTY_LIST = ${CMAKE_PROPERTY_LIST}")
+endfunction()
+    
+function(print_target_properties target)
+# Get all propreties that cmake supports
+    if (NOT TARGET ${target})
+      message(STATUS "There is no target named '${target}'")
+      return()
+    endif()
+
+    if (NOT CMAKE_PROPERTY_LIST)
+        execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
+        # Convert command output into a CMake list
+        string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
+        string(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
+    endif()
+
+    foreach (property ${CMAKE_PROPERTY_LIST})
+        string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" property ${property})
+        if (property STREQUAL "LOCATION" OR property MATCHES "^LOCATION_" OR property MATCHES "_LOCATION$")
+            continue()
+        endif()
+
+        get_property(was_set TARGET ${target} PROPERTY ${property} SET)
+        if (was_set)
+            get_target_property(value ${target} ${property})
+            message("${target} ${property} = ${value}")
+        endif()
+    endforeach()
+endfunction()
