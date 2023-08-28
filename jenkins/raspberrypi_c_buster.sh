d6e60925d951b8f3f4b63af6c7c305e9427199dd
Eric Wolz <ericwol@microsoft.com>
2023-08-28 14:56:10 -0700

    print PI version

--- jenkins/raspberrypi_c_buster.sh
+++ jenkins/raspberrypi_c_buster.sh
@@ -7,6 +7,12 @@ set -o errexit # Exit if command failed
 set -o nounset # Exit if variable not set
 set -o pipefail # Exit if pipe failed
 
+# Print version
+cat /etc/*release | grep VERSION*
+gcc --version
+openssl version
+curl --version
+
 # assume directory is root of downloaded repo
 mkdir cmake
 cd cmake
