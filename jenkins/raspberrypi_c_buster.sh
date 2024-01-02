ef7b33c8178238eb121b9d4b596642664b4236a3
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2024-01-02 13:37:43 -0800

    Update gate build scripts (sync to main)

--- jenkins/raspberrypi_c_buster.sh
+++ jenkins/raspberrypi_c_buster.sh
@@ -7,6 +7,11 @@ set -o errexit # Exit if command failed
 set -o nounset # Exit if variable not set
 set -o pipefail # Exit if pipe failed
 
+# Print version
+cat /etc/*release | grep VERSION*
+gcc --version
+openssl version
+
 # assume directory is root of downloaded repo
 mkdir cmake
 cd cmake
