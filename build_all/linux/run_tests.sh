7e56b06bb73a62344fd1e885d62b1758b95a9fe6
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2026-04-16 13:58:24 -0700

    Reduce parallelism for valgrind/drd/helgrind runs

--- build_all/linux/run_tests.sh
+++ build_all/linux/run_tests.sh
@@ -8,8 +8,14 @@ set -o pipefail # Exit if pipe failed.
 
 # Only for testing E2E behaviour !!! 
 TEST_CORES=16
+VALGRIND_CORES=4
 
 # Refresh dynamic libs to link to
 sudo ldconfig
 
-ctest -T test --no-compress-output -C "Debug" -V -j $TEST_CORES --schedule-random
+# Run non-valgrind tests with full parallelism
+ctest -T test --no-compress-output -C "Debug" -V -j $TEST_CORES --schedule-random -E "_(valgrind|helgrind|drd)$"
+
+# Run valgrind/helgrind/drd tests with reduced parallelism to avoid
+# SSL connection storms from too many concurrent TLS handshakes
+ctest -T test --no-compress-output -C "Debug" -V -j $VALGRIND_CORES --schedule-random -R "_(valgrind|helgrind|drd)$"
