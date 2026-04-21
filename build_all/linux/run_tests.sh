d476e043da3c5ee8c3ba6eb27eb086531cc01d25
Ewerton Scaboro da Silva <ewertons@microsoft.com>
2026-04-21 14:32:18 -0700

    Fix run_tests.sh: ensure valgrind tests run even if non-valgrind tests fail
    The set -o errexit caused the script to abort after the first ctest
    invocation (non-valgrind tests) if any test failed, preventing the
    second ctest invocation (valgrind/helgrind/drd tests) from ever
    running. Replace errexit with explicit exit code tracking so both
    test phases always execute.

--- build_all/linux/run_tests.sh
+++ build_all/linux/run_tests.sh
@@ -3,7 +3,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-set -o errexit # Exit if command failed.
 set -o pipefail # Exit if pipe failed.
 
 # Only for testing E2E behaviour !!! 
@@ -13,9 +12,13 @@ VALGRIND_CORES=4
 # Refresh dynamic libs to link to
 sudo ldconfig
 
+result=0
+
 # Run non-valgrind tests with full parallelism
-ctest -T test --no-compress-output -C "Debug" -V -j $TEST_CORES --schedule-random -E "_(valgrind|helgrind|drd)$"
+ctest -T test --no-compress-output -C "Debug" -V -j $TEST_CORES --schedule-random -E "_(valgrind|helgrind|drd)$" || result=$?
 
 # Run valgrind/helgrind/drd tests with reduced parallelism to avoid
 # SSL connection storms from too many concurrent TLS handshakes
-ctest -T test --no-compress-output -C "Debug" -V -j $VALGRIND_CORES --schedule-random -R "_(valgrind|helgrind|drd)$"
+ctest -T test --no-compress-output -C "Debug" -V -j $VALGRIND_CORES --schedule-random -R "_(valgrind|helgrind|drd)$" || result=$?
+
+exit $result
